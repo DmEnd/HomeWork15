0af49b7c9bc478038e9a77d245ba62fa4dc20a80
DmEnd <Dima_polit-sov@mail.ru>
2022-11-11 01:54:59 +0300

    Добавьте файлы проекта.

---
+++ HomeWork15/Classes.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork15
+{
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+    class ArithProgression : ISeries
+    {
+        int startValue;
+        int currentValue;
+        int step;
+        public int GetNext()
+        {
+            return currentValue += step;
+        }
+
+        public void Reset()
+        {
+            currentValue=startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue=x;
+            currentValue = startValue;
+        }
+        public void SetStep(int step)
+        {
+            this.step = step;
+        }
+    }
+
+    class GeomProgression : ISeries
+    {
+        int startValue;
+        int currentValue;
+        int step;
+        public int GetNext()
+        {
+            return currentValue = currentValue * step;
+        }
+
+        public void Reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void SetStep(int step)
+        {
+            this.step = step;
+        }
+    }
+
+}
---
+++ HomeWork15/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Арифметическая прогрессия");
+            ArithProgression arProg = new ArithProgression();
+            arProg.SetStart(10);
+            arProg.SetStep(2);
+
+            Console.WriteLine(arProg.GetNext());
+            Console.WriteLine(arProg.GetNext());
+            Console.WriteLine(arProg.GetNext());
+
+            arProg.Reset();
+
+            Console.WriteLine(arProg.GetNext());
+            Console.WriteLine(arProg.GetNext());
+
+            Console.WriteLine("Геометрическая прогрессия");
+            GeomProgression geProg = new GeomProgression();
+            geProg.SetStart(3);
+            geProg.SetStep(2);
+
+            Console.WriteLine(geProg.GetNext());
+            Console.WriteLine(geProg.GetNext());
+            Console.WriteLine(geProg.GetNext());
+
+            geProg.Reset();
+
+            Console.WriteLine(geProg.GetNext());
+            Console.WriteLine(geProg.GetNext());
+
+            Console.ReadKey();
+        }
+    }
+}
